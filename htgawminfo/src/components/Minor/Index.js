bc88988a1efe63f64c562c6d89ad036f343c1f02
sisiishereb <set38246@marymount.edu>
2021-08-31 03:16:53 -0400

    updating body font colours and minor and supporting character pages

--- htgawminfo/src/components/Minor/Index.js
+++ htgawminfo/src/components/Minor/Index.js
@@ -6,6 +6,15 @@ const Form = props => {
         <form>
             <h2>Minor Characters and Statuses</h2>
             <list>
+                <li>
+                    <ul>Aiden Walker: Alive</ul>
+                    <ul>Celestine Harkness: Alive</ul>
+                    <ul>Charles Mahoney: Alive</ul>
+                    <ul>Mary Walker: Alive</ul>
+                    <ul>Mr. Delfino: Alive</ul>
+                    <ul>Olivia Pope: Alive</ul>
+                    <ul>Sylvia Mahoney: Alive</ul>
+                </li>
             </list>
         </form>
     )
--- htgawminfo/src/components/supporting/Index.js
+++ htgawminfo/src/components/supporting/Index.js
@@ -6,6 +6,18 @@ const Form = props => {
         <form>
             <h2>Supporting Characters, Occupations, Alignments, and Statuses</h2>
             <list>
+                <li>
+                    <ul>Meggy Travers: Medical Student, Positive Alignment, Alive</ul>
+                    <ul>Lila Stangard: N/A, Positive Alignment, Deceased</ul>
+                    <ul>Ophelia Harkness: N/A, Positive Alignment, Deceased</ul>
+                    <ul>Caleb Hapstall: : N/A, Negative Alignment, Deceased</ul>
+                    <ul>Catherine Hapstall: N/A, Negative Alignment, Alive</ul>
+                    <ul>Christopher Castillo: Law Professor, Positive Alignment, AliveAlive</ul>
+                    <ul>Wallace Mahoney: Businessman, Negative Alignment, Deceased</ul>
+                    <ul>Dominick Flores: Hitman, Negative Alignment, Deceased</ul>
+                    <ul>Jorge Castillo: CEO, Negative Alignment, Deceased</ul>
+                    <ul>Sam Keating: Psychology Professor, Negative Alignment, Deceased</ul>
+                </li>
             </list>
         </form>
     )
